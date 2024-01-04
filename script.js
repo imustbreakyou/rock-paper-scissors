a85665f2c123f8ef7d621d17194f0e4f0a2871ef
mike <8876812+imustbreakyou@users.noreply.github.com>
2024-01-03 16:41:57 -0800

    implement win conditions and visualization for playRound()

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 
+const displayComputerChoice = document.querySelector("#computer-choice");
+const displayUserChoice = document.querySelector("#user-choice");
+const displayRoundOutcome = document.querySelector("#round-outcome");
+
+
 
 document.getElementById('rock').addEventListener('click', function() {
     playRound(this.id);
@@ -36,12 +41,28 @@ function getComputerChoice () {
     }
 
     console.log("computer's choice:", computerChoice);
+    displayComputerChoice.textContent = "Computer Choice: " + computerChoice;
     return computerChoice;
     
 }
 
 function playRound(choice) {
     let userChoice = choice;
+    displayUserChoice.textContent = "User Choice: " + userChoice;
     let computerChoice = getComputerChoice();
+    let outcome;
+
+    if (userChoice == computerChoice) {
+        outcome = 'tie!';
+        displayRoundOutcome.textContent = "Round Outcome: " + outcome;
+    } else if (userChoice == 'rock' && computerChoice == 'scissors' || userChoice == 'paper' && computerChoice == 'rock' ||userChoice == 'scissors' && computerChoice == 'paper') {
+        outcome = 'User Win!';
+        displayRoundOutcome.textContent = "Round Outcome: " + outcome;
+    } else {
+        outcome = 'Computer Win!';
+        displayRoundOutcome.textContent = "Round Outcome: " + outcome;
+    }
+        
+
     console.log("user choice: ", userChoice, " computer Choice:", computerChoice)
 }
